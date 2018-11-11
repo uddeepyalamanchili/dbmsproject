36b8ba58411b906f4acd17bf3ec4b6bc0ac8e61d
UDDIP YALAMANCHILI <31996502+uddeepyalamanchili@users.noreply.github.com>
2018-11-11 12:11:58 +0530

    rectified errors in the ticket

--- confirm4.php
+++ confirm4.php
@@ -9,7 +9,7 @@ session_start();
   <meta name="keywords" content="website keywords, website keywords" />
   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
   <link rel="stylesheet" type="text/css" href="style/style.css" />
-  
+
 </head>
 <script src="validation_signup.js"></script>
 <body style="background-size: 100% 100% ">
@@ -25,28 +25,28 @@ session_start();
 	  <form method="post" action="gen_4.php">
 	  <?php
 
-			$name1=$_POST["name1"];
-			$name2=$_POST["name2"];
-			$name3=$_POST["name3"];
-			$name4=$_POST["name4"];
-			$age1=$_POST["age1"];
-			$age2=$_POST["age1"];
-			$age3=$_POST["age3"];
-			$age4=$_POST["age4"];
-			$gender1=$_POST["GENDER1"];
-			$gender2=$_POST["GENDER2"];
-			$gender3=$_POST["GENDER3"];
-			$gender4=$_POST["GENDER4"];
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
 			$link=mysqli_connect("localhost", "root","jaihindh","user") or die(mysql_error());
 
 
-		
+
 			//$dbase_name="user";
 
-			//mysql_select_db($dbase_name) or die(mysql_error()); 
+			//mysql_select_db($dbase_name) or die(mysql_error());
 			$data=mysqli_query($link,"select * from trains where Train_name='GUJRAT MAIL' ")or die(mysqli_error());
-			
+
 			while($info=mysqli_fetch_array($data))
 			{
 			print"<table>";
@@ -76,41 +76,53 @@ session_start();
 					print"<tr><td><u>Passenger$ps</u></td></tr>";
 					switch ($ps) {
 								case 1:
+                  $name1=$_POST["name1"];
+                  $age1=$_POST["age1"];
+                  $gender1=$_POST["GENDER1"];
 									print"<p>Name :$name1</p>";
 									print"<p>Age :$age1</p><br>";
 									$_SESSION["panme1"]=$name1;
 									$_SESSION["age1"]=$age1;
-									
+
 									break;
 								case 2:
+                  $name2=$_POST["name2"];
+                  $age2=$_POST["age1"];
+                  $gender2=$_POST["GENDER2"];
 									print"<p>Name :$name2</p>";
 									print"<p>Age :$age2</p>";
 									$_SESSION["panme2"]=$name2;
 									$_SESSION["age2"]=$age2;
 									break;
 								case 3:
+                  $name3=$_POST["name3"];
+                  $age3=$_POST["age3"];
+                  $gender3=$_POST["GENDER3"];
 									print"<p>Name :$name3</p>";
 									print"<p>Age:$age3</p>";
 									$_SESSION["panme3"]=$name3;
 									$_SESSION["age3"]=$age3;
 									break;
 								case 4:
+                  $name4=$_POST["name4"];
+                  $age4=$_POST["age4"];
+                  $gender4=$_POST["GENDER4"];
 									print"<p>Name :$name4</p>";
 									print"<p>Age :$age4</p>";
 									$_SESSION["panme4"]=$name4;
 									$_SESSION["age4"]=$age4;
 									break;
 								}
-					
+
 				}
-				
+
 			}
 			print"<p><button id='registerNew' type='submit' >Confirm</button></p>";
-			
+
 		?>
 		</FORM>
 	  </div>
-	  
+
 	  <div id="content_footer" style="padding-top:60px"></div>
     <div id="footer">
       <p><a href="index.html">Home</a> | <a href="examples.html">Our Cities</a> | <a href="safety.html">Safety</a> | <a href="help.html">Help Center</a> | <a href="contact.html">Contact Us</a> |</p>
